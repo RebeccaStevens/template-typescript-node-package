47bf18655fff13da35456bcbfab2fdb63daa81c9
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2024-03-15 18:40:50 +1300

    chore: remove downstream project test setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,30 +1,11 @@
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
-/**
- * Get the intended boolean value from the given string.
- */
-function getBoolean(value: string | undefined) {
-  if (value === undefined) {
-    return false;
-  }
-  const asNumber = Number(value);
-  return Number.isNaN(asNumber)
-    ? Boolean(String(value).toLowerCase().replace("false", ""))
-    : Boolean(asNumber);
-}
-
-const useCompiledTests = getBoolean(process.env["USE_COMPILED_TESTS"]);
-
-const testFilePattern = `${
-  useCompiledTests ? "./tests-compiled" : "."
-}/**/*.test.${useCompiledTests ? "js" : "ts"}`;
-
 export default defineConfig({
   plugins: [tsconfigPaths()],
 
   test: {
-    include: [testFilePattern],
+    include: ["./**/*.test.ts"],
     exclude: ["dist", "node_modules"],
     coverage: {
       all: true,
